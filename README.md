45026adf13f2e3940cd776a9784b905ffff6ea8a
Gopi Srinivasan <igopisrinivasan@gmail.com>
2020-06-30 22:35:59 +0530

    Dummy project added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# playwrightPractise
+# Playwright e2e Automated Testing Demo
+
+A demo project that demonstrates how to use [Playwright](https://github.com/microsoft/playwright) and how to run it using CircleCI.
+
+Build status: [![CircleCI](https://circleci.com/gh/alisterscott/playwright-demo/tree/master.svg?style=svg)](https://circleci.com/gh/alisterscott/playwright-demo/tree/master)
+
+## Prerequisites
+
+#### Make sure you have node-js installed, available via homebrew
+
+`npm install`
+
+#### To run the specs
+
+`npm test`
---
+++ index.js
@@ -0,0 +1,25 @@
+import playwright from 'playwright';
+import config from 'config';
+
+const mochaTimeoutMS = config.get( 'mochaTimeoutMS' );
+
+describe( 'Playwright 1', function() {
+	this.timeout( mochaTimeoutMS );
+
+	let browser, context;
+
+	before( async function() {
+		browser = await playwright.chromium.launch();
+		context = await browser.newContext();
+	} );
+
+	it( 'can wait for an element to appear', async function() {
+		const page = await context.newPage( `${ config.get( 'baseURL' )}` );
+		await page.waitForSelector( '#elementappearschild', { visible: true, timeout: 5000 } );
+	} );
+
+	after( async function() {
+		await browser.close();
+	} );
+
+} );
